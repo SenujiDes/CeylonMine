8e39cb0262a242a640b3834b69fd3b68870625d6
Janindu Amaraweera <janiduamaraweera@gmail.com>
2025-01-03 13:26:54 +0530

    second

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../navbar/page'
+
+export default function about() {
+  return (
+    <main>
+      <Navbar/>
+      <div>about</div>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum rem voluptatum saepe incidunt! Quidem, assumenda incidunt mollitia soluta necessitatibus natus quasi fuga esse dolorem, ipsa, ex quae in doloremque cupiditate.</p>
+    </main>
+  )
+}
