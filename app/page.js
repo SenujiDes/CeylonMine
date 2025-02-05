65eb6bfe29debba41feee82f8a4a422e063e0dc7
Minsandi De Silva <163978454+MinsandiDeSilva@users.noreply.github.com>
2025-02-05 18:20:29 +0530

    Add files via upload
    supportPage_frontend

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ComplaintForm from "./components/ComplaintForm";
+//import ComplaintList from "./components/ComplaintList";
+import "./globals.css";
+export default function Home() {
+  return (
+    <>
+      <ComplaintForm/>
+      
+    </>
+  );
+}
