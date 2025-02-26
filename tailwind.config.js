79d4d6df479cc73a6774b492ad4ccb2acb30cd85
Thisal03 <w2052010@westminster.ac.uk>
2025-02-26 19:32:00 +0530

    Added corrected frontend

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
-module.exports = {
-  content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
+//module.exports = {
+//  content: [
+//    './app/**/*.{js,ts,jsx,tsx,mdx}',
+//    './components/**/*.{js,ts,jsx,tsx,mdx}',
+//  ],
+/*  theme: {
     extend: {
       colors: {
         primary: {
@@ -16,4 +16,16 @@ module.exports = {
     },
   },
   plugins: [],
-}
+}*/
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
