dcafb0c5e3e3e949985d7034f3e1d869edb21329
nisil <151009435+nisilliyanage@users.noreply.github.com>
2025-03-04 00:06:41 +0530

    Add files via upload

--- page.js
+++ page.js
@@ -39,6 +39,7 @@ export default function EducationalPage() {
           <Tab label="Mineral Royalties" />
           <Tab label="FAQs" />
           <Tab label="License Categories" />
+          <Tab label="Licensing Process" />
         </Tabs>
 
         {/* Content Sections */}
@@ -82,7 +83,7 @@ export default function EducationalPage() {
 
         <TabPanel value={selectedTab} index={1}>
           <Typography variant="h5" gutterBottom>
-            Environmental Compliance
+            
           </Typography>
           <Typography paragraph>
             1. Environmental Regulation<br/>
@@ -136,6 +137,40 @@ export default function EducationalPage() {
           <b>3. Trading License (TDL)</b><br/>
           <b>4. Transport License (TL)</b><br/>
           <b>5. Export of Minerals</b><br/>
+          </Typography>
+        </TabPanel>
+
+        <TabPanel value={selectedTab} index={5}>
+        <Typography variant="h5" gutterBottom>
+            Issuarance of license
+          </Typography>
+          <Typography paragraph>
+          <b> Inspection of the site and suggestions: </b><br/>
+          
+      
+          A site engineer is assigned by GSMB to examine the proposed mining location following the submission of an application. The engineer evaluates a number of factors, including the land's suitability for mining and any possible environmental effects. Additionally, the engineer could suggest other reports or clearances that the applicant requires to get the following documents;<br/>
+          1. Central Environment Authority (CEA) Environmental Report:- <br/>
+          To ensure that local ecosystems won't be harmed by the proposed mining. <br/>
+          
+          2. Landowners' Consent:- <br/>
+          To verify the landowners' permission for the mining operations.<br/>
+          
+          3. Grama Niladhari and Police Reports:- <br/>
+          These reports help in confirming the applicant's background and ensuring support from the local community. <br/>
+          
+          4. Water Board and UDA approvals:- <br/>
+          To ensure that plans for urban growth and water resources won't be impacted by the mining operation.<br/>
+              
+          5. Detailed Site Map:- <br/>
+          Provides a map layout of the mining site for review. <br/><br/>
+
+          <b> Screening for Eligibility: </b><br/>
+          There are restrictions on those who can apply for a mining license. To verify compliance with rules, GSMB runs background checks on applicants. For example, certain individuals may not be eligible for a license, such as government officials, those in financial instability, or people with a history of non-compliance. In order to preserve these ecosystems, licenses are also not issued in protected places like livestock reserves or historical landmarks.<br/>
+          
+          <br/><b> Approval and Issuance: </b><br/>
+          The license can be issued after every document has been reviewed and the required approvals have been secured. However, because each step is done manually and communication between sub-offices and the head office is delayed, applicants frequently have to visit the GSMB office more than once. Due to this drawn-out procedure and the absence of a centralized database, applicants experience significant delays and find it challenging to follow their applications.<br/>
+
+        
           </Typography>
         </TabPanel>
       </Paper>
