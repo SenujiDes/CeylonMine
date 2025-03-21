e59ce6a458e73020c03ccd379e8fbda21c03bb36
Janindu Amaraweera <janiduamaraweera@gmail.com>
2025-03-21 17:52:28 +0530

    error fixing

--- adminroyalty/app/api/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-  return NextResponse.json({
-    status: "healthy",
-    message: "Explosives Royalty Calculator API",
-    version: "1.0.0",
-  });
-} 
