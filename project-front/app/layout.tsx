2edb35b013db99918ddf8750df06a2456ce449f1
Thisal Induwara <w2052010@westminster.ac.uk>
2025-03-22 16:42:06 +0530

    Update layout.tsx

--- project-front/app/layout.tsx
+++ project-front/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Roboto_Mono } from 'next/font/google';
 import "./globals.css";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ['latin'] });
 const robotoMono = Roboto_Mono({ subsets: ['latin'] });
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Toaster position="top-right" />
         {children}
       </body>
     </html>
