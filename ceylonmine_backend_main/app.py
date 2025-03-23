648611eecab9ba81c84c9a8a0637a64b4994c033
Minsandi De Silva <163978454+MinsandiDeSilva@users.noreply.github.com>
2025-03-23 09:20:06 +0530

    Updated app.py

--- ceylonmine_backend_main/app.py
+++ ceylonmine_backend_main/app.py
@@ -8,5 +8,5 @@ def home():
     return "Hello, World!"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Use PORT if set, otherwise default to 5000
+    port = int(os.environ.get("PORT", 5000))  
     app.run(host="0.0.0.0", port=port)
