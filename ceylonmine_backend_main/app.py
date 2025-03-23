c76b3e0ebc04b88fb04ff93f9c566755c336facf
Minsandi De Silva <163978454+MinsandiDeSilva@users.noreply.github.com>
2025-03-23 09:19:45 +0530

    changes the port to fix an error when deploying

--- ceylonmine_backend_main/app.py
+++ ceylonmine_backend_main/app.py
@@ -1,6 +1,12 @@
-from __init__ import create_app
+import os
+from flask import Flask
 
-app = create_app()
+app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/')
+def home():
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use PORT if set, otherwise default to 5000
+    app.run(host="0.0.0.0", port=port)
